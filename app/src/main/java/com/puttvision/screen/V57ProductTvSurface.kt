c554ebccf388f9d7c3ba61d540873bebd599d410
mercurial0416-lgtm <mercurial0416@gmail.com>
2026-08-16 19:51:46 +0900

    V90: mount cinematic director on canonical TV

--- app/src/main/java/com/puttvision/screen/V57ProductTvSurface.kt
+++ app/src/main/java/com/puttvision/screen/V57ProductTvSurface.kt
@@ -18,6 +18,7 @@ object V57ProductTvSurface {
         setBackgroundColor(Color.BLACK)
         addView(V18SimulatorFactory.create(context, engine), FrameLayout.LayoutParams(-1, -1))
         addView(V89ScreenGolfVisualPhysicsView(context, engine), FrameLayout.LayoutParams(-1, -1))
+        addView(V90ScreenGolfCinematicOverlay(context, engine), FrameLayout.LayoutParams(-1, -1))
         addView(V51TvPolishOverlay(context, engine), FrameLayout.LayoutParams(-1, -1))
         addView(V87TvVisualPolishView(context, engine), FrameLayout.LayoutParams(-1, -1))
         addView(V27PaceLineOverlay(context, engine), FrameLayout.LayoutParams(-1, -1))
