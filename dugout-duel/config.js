303f42b3275b8f2471a8836b39de847304e9fc95
mercurial0416-lgtm <mercurial0416@gmail.com>
2026-08-31 10:08:26 +0900

    Configure Dugout Duel Supabase endpoint

---
+++ dugout-duel/config.js
@@ -0,0 +1,5 @@
+window.DUGOUT_CONFIG = {
+  supabaseUrl: 'https://nwenbkthlpzlpfklgonb.supabase.co',
+  // Supabase publishable keys are designed to be used in browser clients.
+  publishableKey: 'sb_publishable_bzGqbB-bPSO3wHurZDlAgg_Qht6OyNS'
+};
