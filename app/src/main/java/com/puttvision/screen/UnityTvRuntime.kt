4b2d4dc9b483a9479ab3112dba8cca64c7c47691
mercurial0416-lgtm <mercurial0416@gmail.com>
2026-09-08 09:07:03 +0900

    fix: make Unity launch failure terminal

--- app/src/main/java/com/puttvision/screen/UnityTvRuntime.kt
+++ app/src/main/java/com/puttvision/screen/UnityTvRuntime.kt
@@ -130,6 +130,7 @@ object UnityTvRuntime {
             if (!launchSessions.matches(displayId, launchSession)) return
             setupComplete = false
             lastFailure = message?.takeIf { it.isNotBlank() } ?: "Unity renderer failure"
+            launchSessions.clearIf(displayId, launchSession)
         }
     }
 
