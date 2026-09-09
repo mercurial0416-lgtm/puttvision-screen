f6f6bfab3b7691d19ae0a7d4035c677acc19d929
mercurial0416-lgtm <mercurial0416@gmail.com>
2026-09-10 00:08:40 +0900

    Fix cleanup regression test literals

--- app/src/test/java/com/puttvision/screen/CurrentMainReleaseCleanupRegressionTest.kt
+++ app/src/test/java/com/puttvision/screen/CurrentMainReleaseCleanupRegressionTest.kt
@@ -17,14 +17,14 @@ class CurrentMainReleaseCleanupRegressionTest {
     @Test
     fun publishedReleaseIsCleanedIfFailureHappensBeforeUpdaterPublish() {
         val workflow = workflow()
-        val publish = workflow.indexOf("echo \"PV_RELEASE_PUBLISHED=true\" >> \"$GITHUB_ENV\"")
+        val publish = workflow.indexOf("echo \"PV_RELEASE_PUBLISHED=true\" >> \"\$GITHUB_ENV\"")
         val cleanup = workflow.indexOf("Remove unpublished stale release on pre-updater failure")
         val updater = workflow.indexOf("Publish and verify public updater manifest")
 
         assertTrue("release publication marker must exist", publish >= 0)
         assertTrue("cleanup step must exist after GitHub release publication", cleanup > publish)
         assertTrue("cleanup must run before updater publication", updater > cleanup)
-        assertTrue(workflow.contains("if: ${{ failure() && env.PV_RELEASE_PUBLISHED == 'true' }}"))
-        assertTrue(workflow.contains("gh release delete \"$PV_TAG\" --cleanup-tag --yes"))
+        assertTrue(workflow.contains("if: \${{ failure() && env.PV_RELEASE_PUBLISHED == 'true' }}"))
+        assertTrue(workflow.contains("gh release delete \"\$PV_TAG\" --cleanup-tag --yes"))
     }
 }
