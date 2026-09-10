0de2a42ab92629852e03fe718530195638728f53
mercurial0416-lgtm <mercurial0416@gmail.com>
2026-09-11 06:09:16 +0900

    Test ambiguous public updater paths

--- app/src/test/java/com/puttvision/screen/TrustedManifestEndpointRegressionTest.kt
+++ app/src/test/java/com/puttvision/screen/TrustedManifestEndpointRegressionTest.kt
@@ -6,6 +6,7 @@ import org.junit.Test
 
 class TrustedManifestEndpointRegressionTest {
     private val manifest = "https://example.com/update.json"
+    private val publicApk = "https://github.com/mercurial0416-lgtm/puttvision-screen/releases/download/pv-100552/puttvision-consumer.apk"
 
     @Test
     fun manifestTransportRejectsQueryAndNonStandardHttpsPort() {
@@ -14,5 +15,21 @@ class TrustedManifestEndpointRegressionTest {
 
         assertFalse(V49UpdatePolicy.validateManifestUrl("$manifest?cache=bust").valid)
         assertFalse(V49UpdatePolicy.validateManifestUrl("https://example.com:444/update.json").valid)
+    }
+
+    @Test
+    fun publicApkRejectsAmbiguousEncodedAndDotSegmentPaths() {
+        assertTrue(V49UpdatePolicy.validatePublicApkUrl(publicApk).valid)
+
+        assertFalse(
+            V49UpdatePolicy.validatePublicApkUrl(
+                "https://github.com/mercurial0416-lgtm/puttvision-screen/releases/download/pv-100552/%70uttvision-consumer.apk"
+            ).valid
+        )
+        assertFalse(
+            V49UpdatePolicy.validatePublicApkUrl(
+                "https://github.com/mercurial0416-lgtm/puttvision-screen/releases/download/pv-100552/../puttvision-consumer.apk"
+            ).valid
+        )
     }
 }
